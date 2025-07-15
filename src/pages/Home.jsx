e8d19b56281483aa5683aebdd892c48fb3db2164
DevangPatel2006 <girapatel361@gmail.com>
2025-07-15 15:02:22 +0530

    Initial commit 🚀

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/HEro'
+import AiTools from '../components/AiTools'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <AiTools/>
     </div>
   )
 }
