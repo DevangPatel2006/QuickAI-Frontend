450ed7f96d13bbccebe5ae75663aa6ac3ed0cf6d
DevangPatel2006 <girapatel361@gmail.com>
2025-07-15 18:43:07 +0530

    Initial commit 🚀

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
 const Dashboard = () => {
+  const [creations, setCreations] = useState([]);
+  const getDashboardData = async () => {
+    setCreations(dummyCreationData);
+  };
+  useEffect(()=>{
+    getDashboardData()
+  },[])
   return (
     <div>
-      
+      <h1>Dashboard</h1>
     </div>
-  )
-}
-
-export default Dashboard
+  );
+};
+export default Dashboard;
